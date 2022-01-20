d1be25adc673ab08cccd9a674fda97d52dee670a
Stephan Gabler <stephan.gabler@gmail.com>
2022-01-20 15:43:58 +0100

    Add buttons to trigger an error

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      {/* @ts-ignore: Line is supposed to trigger an error */}
+      <Button title="Trigger sync error" onPress={() => asdf.syncErrorTest} />
+      <Button
+        title="Trigger async error"
+        onPress={async () =>
+          // @ts-ignore: Line is supposed to trigger an error
+          asdf.ayncErrorTest
+        }
+      />
+
       <StatusBar style="auto" />
     </View>
   );
